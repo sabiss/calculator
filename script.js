c03e8273171e9b5b6882d9745647cb877180763f
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2022-01-20 10:10:29 -0300

    fixing the listeners

--- script.js
+++ script.js
@@ -1,59 +1,87 @@
 window.alert('Página ainda em construção! JavaScript ainda não inserido')
-var zero = document.querySelector("div.zero")
-var um = document.querySelector("div.um")
-var dois = document.querySelector("div.dois")
-var tres = document.querySelector("div.tres")
-var quatro = document.querySelector("div.quatro")
-var cinco = document.querySelector("div.cinco")
-var seis = document.querySelector("div.seis")
-var sete = document.querySelector("div.sete")
-var oito = document.querySelector("div.oito")
-var nove = document.querySelector("div.nove")
+var numero_zero = document.querySelector("div.zero")
+var numero_um = document.querySelector("div.um")
+var numero_dois = document.querySelector("div.dois")
+var numero_tres = document.querySelector("div.tres")
+var numero_quatro = document.querySelector("div.quatro")
+var numero_cinco = document.querySelector("div.cinco")
+var numero_seis = document.querySelector("div.seis")
+var numero_sete = document.querySelector("div.sete")
+var numero_oito = document.querySelector("div.oito")
+var numero_nove = document.querySelector("div.nove")
 
-zero.addEventListener("click", zero)
-um.addEventListener("click", um)
-dois.addEventListener("click", dois)
-tres.addEventListener("click", tres)
-quatro.addEventListener("click", quatro)
-cinco.addEventListener("click", cinco)
-seis.addEventListener("click", seis)
-sete.addEventListener("click", sete)
-oito.addEventListener("click", oito)
-nove.addEventListener("click", nove)
+numero_zero.addEventListener("click", zero)
+numero_um.addEventListener("click", um)
+numero_dois.addEventListener("click", dois)
+numero_tres.addEventListener("click", tres)
+numero_quatro.addEventListener("click", quatro)
+numero_cinco.addEventListener("click", cinco)
+numero_seis.addEventListener("click", seis)
+numero_sete.addEventListener("click", sete)
+numero_oito.addEventListener("click", oito)
+numero_nove.addEventListener("click", nove)
 
 var numero = null
+var visor = document.querySelector("div.calculadora-visor")
+
+function verificar(digito, numero){
+    if(numero == null){
+        numero = digito
+    }
+    else{
+        numero = numero + digito
+    }
+    return numero
+}
 
 function zero(){
-    numero = "0"
-    var visor = document.querySelector("div.calculadora-visor")
-    visor.innerHtml = `${numero}`
-    console.log(numero)
+    var mostrar_visor = verificar("0", numero)
+    visor.innerHTML = mostrar_visor
+    console.log(mostrar_visor)
 }
 function um(){
-    numero = "1"
+    let mostrar_visor = verificar("1", numero)
+    visor.innerHTML = mostrar_visor
+    console.log(numero)
 }
 function dois(){
     numero = "2"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 function tres(){
     numero = "3"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 function quatro(){
     numero = "4"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 function cinco(){
     numero = "5"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 function seis(){
     numero = "6"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 function sete(){
     numero = "7"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 function oito(){
     numero = "8"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 function nove(){
     numero = "9"
+    visor.innerHTML = numero
+    console.log(numero)
 }
 console.log("oi")
