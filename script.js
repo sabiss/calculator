5b3df99b0b99de3600c453d05f88c741b6d81549
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2022-01-29 15:14:40 -0300

    creating calculator functions onclick functions

--- script.js
+++ script.js
@@ -22,7 +22,11 @@ numero_oito.addEventListener("click", oito)
 numero_nove.addEventListener("click", nove)
 
 var numero = null
-var visor = document.querySelector("div.calculadora-visor")
+var numero_auxiliar = null
+var operacao
+var visor_inferior = document.querySelector("div.visor-inferior")
+var visor_superior = document.querySelector("div.visor-superior")
+let porcentagem_teste = false
 
 function verificar(digito, numero){//verificar se já tem um algoritmo em NUMERO para concatenar ou não
     if(numero == null){
@@ -33,65 +37,98 @@ function verificar(digito, numero){//verificar se já tem um algoritmo em NUMERO
     }
     return numero
 }
-
+//funções para definir os números
 function zero(){
     let mostrar_visor = verificar("0", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function um(){
     let mostrar_visor = verificar("1", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function dois(){
     let mostrar_visor = verificar("2", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function tres(){
     let mostrar_visor = verificar("3", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function quatro(){
     let mostrar_visor = verificar("4", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function cinco(){
     let mostrar_visor = verificar("5", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function seis(){
     let mostrar_visor = verificar("6", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function sete(){
     let mostrar_visor = verificar("7", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function oito(){
     let mostrar_visor = verificar("8", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
 function nove(){
     let mostrar_visor = verificar("9", numero)
+    numero_auxiliar = mostrar_visor
     numero = mostrar_visor
-    visor.innerHTML = mostrar_visor
+    visor_inferior.innerHTML = mostrar_visor
     console.log(mostrar_visor)
 }
-console.log("oi")
+//funções das funções da calculadora
+function zerar(){
+    visor_inferior.innerHTML = "";
+    visor_superior.innerHTML = "";
+    numero = null;
+}
+
+function deletar(){
+    numero = numero.slice(0, -1);
+    visor_inferior.innerHTML = numero;
+}
+function colocar_ponto(){
+    numero = numero + "."
+    visor_inferior.innerHTML = numero;
+    console.log(numero)
+}
+function resultado(){
+    visor_inferior.innerHTML = operacao
+}
+//operações da calculadora
+function porcentagem(){//em construção
+    visor_superior.innerHTML = `${numero}%`
+    visor_inferior.innerHTML = ""
+}
