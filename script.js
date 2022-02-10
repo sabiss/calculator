c83a680f683513e1008bc5a0578ed83a41b6307e
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2022-02-09 21:32:48 -0300

    improving the logic of functions making them easier

--- script.js
+++ script.js
@@ -1,134 +1,130 @@
 window.alert('Página ainda em construção! JavaScript ainda não inserido totalmente')
-var numero_zero = document.querySelector("div.zero")
-var numero_um = document.querySelector("div.um")
-var numero_dois = document.querySelector("div.dois")
-var numero_tres = document.querySelector("div.tres")
-var numero_quatro = document.querySelector("div.quatro")
-var numero_cinco = document.querySelector("div.cinco")
-var numero_seis = document.querySelector("div.seis")
-var numero_sete = document.querySelector("div.sete")
-var numero_oito = document.querySelector("div.oito")
-var numero_nove = document.querySelector("div.nove")
 
-numero_zero.addEventListener("click", zero)
-numero_um.addEventListener("click", um)
-numero_dois.addEventListener("click", dois)
-numero_tres.addEventListener("click", tres)
-numero_quatro.addEventListener("click", quatro)
-numero_cinco.addEventListener("click", cinco)
-numero_seis.addEventListener("click", seis)
-numero_sete.addEventListener("click", sete)
-numero_oito.addEventListener("click", oito)
-numero_nove.addEventListener("click", nove)
+const visor_inferior = document.querySelector('div.visor-inferior')
+const visor_superior = document.querySelector('div.visor-superior')
 
-var numero = null
-var numero_auxiliar = null
-var operacao
-var visor_inferior = document.querySelector("div.visor-inferior")
-var visor_superior = document.querySelector("div.visor-superior")
-let porcentagem_teste = false
-
-function verificar(digito, numero){//verificar se já tem um algoritmo em NUMERO para concatenar ou não
-    if(numero == null){
-        numero = digito
+let digito = null;
+function verif_tela(elemento){
+    if(elemento == null || elemento == ""){
+        return true//não foi digitado nada ainda
     }
     else{
-        numero = numero + digito
+        return false//já tem digitos guardados
     }
-    return numero
 }
-//funções para definir os números
-function zero(){
-    let mostrar_visor = verificar("0", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function um(){
-    let mostrar_visor = verificar("1", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function dois(){
-    let mostrar_visor = verificar("2", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function tres(){
-    let mostrar_visor = verificar("3", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function quatro(){
-    let mostrar_visor = verificar("4", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function cinco(){
-    let mostrar_visor = verificar("5", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function seis(){
-    let mostrar_visor = verificar("6", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function sete(){
-    let mostrar_visor = verificar("7", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function oito(){
-    let mostrar_visor = verificar("8", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-function nove(){
-    let mostrar_visor = verificar("9", numero)
-    numero_auxiliar = mostrar_visor
-    numero = mostrar_visor
-    visor_inferior.innerHTML = mostrar_visor
-    console.log(mostrar_visor)
-}
-//funções das funções da calculadora
-function zerar(){
-    visor_inferior.innerHTML = "";
-    visor_superior.innerHTML = "";
-    numero = null;
-}
+function inserir(parametro){
+    switch(parametro){
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '%':
+        case '.':
+        case '+':
+        case '-':
+        case '÷':
+        case 'x':
+            verif_tela(digito)?digito = parametro: digito = digito + parametro;
+            visor_inferior.innerHTML = digito
+            break;
+        case 'del':
+            verif_tela(digito)?window.alert('Não há número para apagar'):digito = digito.slice(0, -1);
+            visor_inferior.innerHTML = digito
+            break
+        case 'C':
+            visor_inferior.innerHTML = "";
+            visor_superior.innerHTML = "";
+            digito = null
+            break
+        case '=':
+            let acao = null;
+            let acao2 = null;
+            let primeirosNumeros = "", restoNumeros = "";
+            for(let i = 0; i < digito.length; i++){//loop para a verificação da variável ACAO e DIGITO
+                if(acao == null || acao2 == null){//se a ACAO ou ACAO2 ainda não foram encontradas nos DIGITO faça:
+                    if(isNaN(digito[i]) == false || digito[i] == '.'){//verificar se o digito da posicao [i] é um número ou um ponto
+                        if(acao == null){//ver se ACAO ainda não foi preenchida
+                            verif_tela(primeirosNumeros)? primeirosNumeros = digito[i] : primeirosNumeros += digito[i]
+                            //ACAO não preenchida. Primeiros números que estão sendo digitados são guardados em PRIMEIROSNUMEROS
+                        }
+                        else{//ACAO já foi preenchida
+                            verif_tela(restoNumeros)? restoNumeros = digito[i] : restoNumeros += digito[i]
+                            //guardar o resto dos números digitados
+                        }
+                    }
+                    else{
+                        acao == null? acao = digito[i] : acao2 = digito[i]//teste para atribuição das operações
+                    }
+                }
+            }
+            //transformando de String para números
+            primeirosNumeros = Number(primeirosNumeros)
+            restoNumeros = Number(restoNumeros)
 
-function deletar(){
-    numero = numero.slice(0, -1);
-    visor_inferior.innerHTML = numero;
-}
-function colocar_ponto(){
-    numero = numero + "."
-    visor_inferior.innerHTML = numero;
-    console.log(numero)
-}
-function resultado(){
-    visor_inferior.innerHTML = operacao
-}
-//operações da calculadora
-function porcentagem(){//em construção
-    visor_superior.innerHTML = `${numero}%`
-    visor_inferior.innerHTML = ""
+            function formatacao_operacoes(){//mostra na tela a operação desejada
+                if(acao2 == null){
+                    visor_superior.innerHTML = String(primeirosNumeros) + acao + String(restoNumeros)//mostra a operação no visor de cima
+                }
+                else{
+                    visor_superior.innerHTML = String(primeirosNumeros) + acao + String(restoNumeros) + acao2//mostra a operação no visor de cima
+                }
+                digito = null//zera os digitos
+            }
+
+            if(acao != null && acao2 == null){// so foi resgistrada somente uma ação
+                switch(acao){
+                    case '%':
+                        formatacao_operacoes()
+                        visor_inferior.innerHTML = (primeirosNumeros*restoNumeros)/100//mostra o resultado no visor de baixo
+                        break
+                    case '.':
+                        digito = digito + '.'
+                        break
+                    case '+':
+                        formatacao_operacoes()
+                        visor_inferior.innerHTML = primeirosNumeros+restoNumeros
+                        break
+                    case '-':
+                        formatacao_operacoes()
+                        visor_inferior.innerHTML = primeirosNumeros-restoNumeros
+                        break
+                    case '÷':
+                        formatacao_operacoes()
+                        visor_superior.innerHTML = String(primeirosNumeros) + acao + String(restoNumeros)
+                        visor_inferior.innerHTML = primeirosNumeros/restoNumeros
+                        break
+                    case 'x':
+                        formatacao_operacoes()
+                        visor_inferior.innerHTML = primeirosNumeros*restoNumeros
+                        break
+                }
+            }
+            else{//tem duas ações registradas
+                switch(acao2){
+                    case '%':
+                        let porcentagem = "", resultado = "";
+                        switch(acao){
+                            case '-':
+                                formatacao_operacoes()
+                                porcentagem = (primeirosNumeros*restoNumeros)/100
+                                resultado = primeirosNumeros - porcentagem
+                                visor_inferior.innerHTML = resultado//mostra o resultado no visor de baixo
+                                break
+                            case '+':
+                                formatacao_operacoes()
+                                porcentagem = (primeirosNumeros*restoNumeros)/100
+                                resultado = primeirosNumeros + porcentagem
+                                visor_inferior.innerHTML = resultado//mostra o resultado no visor de baixo
+                                break
+                        }
+                        break
+                }
+            }
+    }
 }
